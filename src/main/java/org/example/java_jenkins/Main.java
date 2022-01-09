a083dde5bf972a2bb1f667b3652b3024a8451e9f
Abhishek Verma <abhishekvermamig21@gmail.com>
2022-01-09 18:14:55 +0530

    Bla bla bla

---
+++ src/main/java/org/example/java_jenkins/Main.java
@@ -0,0 +1,16 @@
+package org.example.java_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+    	 System.out.println(checkIfInputIsAnEvenNumber(122)); // Testing 
+    }
+    public static boolean checkIfInputIsAnEvenNumber(int number){
+        return number % 2 == 0;
+    }
+}
